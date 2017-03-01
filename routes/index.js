63d2152f43b9b15217b29da984ea88b816888ee3
Cancel83 <san.egorov@gmail.com>
2017-03-02 01:40:27 +0500

    2222222

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var extsql = require('sqlwork.js');
+var extsql = require('../sqlwork.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Актированные дни' });
   var to = req.query.town;
   var cl = req.query.class;
   var sm = req.query.smena;
-  extsql.sqlwork(to,cl,sm);
-  console.log(to,cl,sm);
+  console.log('dfgdsfg');
+  if ((to != NULL)&&(cl != NULL)&&(sm != NULL)){
+    console.log(to,cl,sm);
+    extsql.xxx(to,cl,sm);
+  }
+
+  //console.log(to,cl,sm);
 });
 
 module.exports = router;
+
--- sqlwork.js
+++ sqlwork.js
@@ -5,21 +5,35 @@ var sqlite3 = require('sqlite3').verbose();
 var mydb = "aktirovka.db";
 var db = new sqlite3.Database(mydb);
 //new function sqlwork(to,cl,sm) {
-
-db.serialize(function() {
-    var id = '1';
-    var to = '3466';
-    var da = '01.01.2017';
-    var sm = '2';
-    var cl = '8';
-    //db.run('CREATE TABLE lorem (info TEXT)');
-    db.run('INSERT INTO aktirovka_days VALUES (:id, :to, :da, :sm, :cl)');
-    //stmt.run();
-    //db.finalize();
-
-    db.each('SELECT * FROM aktirovka_days', function(err, row) {
-        console.log(row.akt_id + ': ' + row.city + ': ' + row.date + ': ' + row.smena + ': ' + row.class);
+function showdate() {
+    var dt = new Date();
+    var day = dt.getDate();
+    var month = dt.getMonth() + 1;
+    var year = dt.getFullYear();
+    if (month <= 9) month = '0' + month;
+    if (day <= 9) day = '0' + day;
+    var cooldate = year + "." + month + "." +day;
+    return cooldate;
+};
+    db.serialize(function xxx(to, cl, sm) {
+        var id = '';
+        //var to = '3465';
+        var da = showdate();
+        //var sm = '2';
+        //var cl = '8';
+        if ((to != NULL)&&(cl != NULL)&&(sm != NULL)) {
+            db.run('INSERT INTO aktirovka_days (city, date, smena, class) VALUES (?,?,?,?)', to, da, sm, cl);
+        }
+        //db.finalize();
+        db.each('SELECT * FROM aktirovka_days', function (err, row) {
+            /*        if (err)
+             {
+             res.render('index');
+             }
+             else*/
+            console.log(row.akt_id + ': ' + row.city + ': ' + row.date + ': ' + row.smena + ': ' + row.class);
+        });
     });
-});
     db.close();
+
 //};
